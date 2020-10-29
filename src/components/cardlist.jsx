ee6b50007c5cc9ea712f77c1849361a5666eba2c
Thoms-C <61702462+Thoms-C@users.noreply.github.com>
2020-10-29 10:48:58 +0100

    fast-search limits

--- src/components/cardlist.jsx
+++ src/components/cardlist.jsx
@@ -4,12 +4,16 @@ import CardItem from './carditem';
 const CardList = ({ cards }) => {
   return (
     <div>
-      {cards.map((card) => {
-        if (card.foreignNames.length === 0) {
-          return null;
-        }
-        return <CardItem key={card.id} {...card} />;
-      })}
+      {cards
+        .filter((card, i) => {
+          return i < 5;
+        })
+        .map((card) => {
+          if (card.foreignNames.length === 0) {
+            return null;
+          }
+          return <CardItem key={card.id} {...card} />;
+        })}
     </div>
   );
 };
--- src/pages/cards.jsx
+++ src/pages/cards.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 import CardList from '../components/cardlist';
-import Spinner from '../components/spinner';
 import SearchRapide from '../components/search-rapide';
 
 class Cards extends React.Component {
@@ -9,24 +8,24 @@ class Cards extends React.Component {
     super();
     this.state = {
       cards: [],
-      loading: false,
     };
   }
 
   searchCard = async (text) => {
-    this.setState({ loading: true });
     const res = await axios.get(
       `https://api.magicthegathering.io/v1/cards?name=${text}`
     );
-    this.setState({ cards: res.data.cards, loading: false });
+    this.setState({ cards: res.data.cards });
   };
 
   render() {
-    const { loading, cards } = this.state;
+    const { cards } = this.state;
     return (
       <div>
         <SearchRapide searchName={this.searchCard} />
-        <div>{loading ? <Spinner /> : <CardList cards={cards} />}</div>
+        <div>
+          <CardList cards={cards} />
+        </div>
       </div>
     );
   }
