fe070d9563dff055206f58e6b15286992d257883
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2020-11-02 18:38:02 +0100

    Change css Mainpage to Style Components CSS

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,75 @@
 import './Main.css';
+import styled from 'styled-components';
 import carteGauche from '../../public/image/carte-gauche.png';
 import carteMilieu from '../../public/image/carte-milieu.png';
 import carteDroite from '../../public/image/carte-droite.png';
+
+const DivFirstSection1 = styled.div`
+  background-image: url(https://images8.alphacoders.com/949/thumb-1920-949830.jpg);
+  height: 90vh;
+  background-position: center;
+`;
+const DivFirstSection2 = styled.div`
+  background-image: url(https://i.ibb.co/YcwdcWf/Zendikar-Rising-resize.png);
+  height: 23vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+`;
+
+const DivSecondSection = styled.div`
+  background-image: url(https://i.ibb.co/pQ9gkgW/zendikar-rising.png);
+  height: 100vh;
+`;
+
+const DivButton = styled.div`
+  text-align: center;
+`;
+
+const DivTxtHistoire = styled.div`
+  padding-top: 10vh;
+  width: 100%;
+  text-align: center;
+`;
+
+const UlCards = styled.ul`
+display: flex;
+justify-content: center;
+}
+`;
+const LiCards = styled.li`
+  height: 10vh;
+  padding: 10vh 8vh 0 8vh;
+`;
+
+const ButtonGrimoire = styled.button`
+margin-top: 40vh;
+background-color: black;
+border-radius: 30px;
+height: 6vh;
+}
+`;
+const A = styled.a`
+  &:hover {
+    color: rgb(255, 123, 0);
+    transition: 0.5s;
+  }
+`;
 
 const MainPage = () => {
   return (
     <div>
       <section>
-        <div className="FirstSection">Aléatoire</div>
-        <div className="FirstSection2">Aléatoire</div>
+        <DivFirstSection1>Aléatoire</DivFirstSection1>
+        <DivFirstSection2>Aléatoire</DivFirstSection2>
       </section>
       <section>
-        <div className="SecondSection">
-          <div className="ContainerTxt">
-            <div className="HistoireTxt">
+        <DivSecondSection>
+          <div>
+            <DivTxtHistoire>
               <h1>Une nouvelle aventure commence</h1>
               <p>
                 Plan tumultueux au mana sauvage, à la météorologie instable et
@@ -24,40 +80,40 @@ const MainPage = () => {
                 pleine d &apos; aventure. Les fleuves venus d &apos; au-dessus
                 de l &apos; horizon tombent en cascade.
               </p>
-            </div>
+            </DivTxtHistoire>
           </div>
 
-          <div className="Cartes">
-            <ul className="CartesHomepage">
-              <li>
+          <div>
+            <UlCards>
+              <LiCards>
                 <img
                   src={carteGauche}
                   alt="Carte de la Canalieuse magmatique"
                   className="Carte"
                 />
-              </li>
-              <li>
+              </LiCards>
+              <LiCards>
                 <img
                   src={carteMilieu}
                   alt="Carte de Jace, mage du miroir"
                   className="Carte"
                 />
-              </li>
-              <li>
+              </LiCards>
+              <LiCards>
                 <img
                   src={carteDroite}
                   alt="Carte du Lac de montagne bouillant"
                   className="Carte"
                 />
-              </li>
-            </ul>
+              </LiCards>
+            </UlCards>
           </div>
-          <div className="Button">
-            <button className="ButtonGrimoire" type="button">
-              <a href="#9">Ouvrir le grimoire de Zendikar</a>
-            </button>
-          </div>
-        </div>
+          <DivButton>
+            <ButtonGrimoire type="button">
+              <A>Ouvrir le grimoire de Zendikar</A>
+            </ButtonGrimoire>
+          </DivButton>
+        </DivSecondSection>
       </section>
     </div>
   );
